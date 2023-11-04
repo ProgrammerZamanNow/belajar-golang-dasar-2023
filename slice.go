f6c1d276141ebc136932f27385b600f0e3a8dcc0
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-04 13:51:57 +0700

    slice

---
+++ slice.go
@@ -0,0 +1,68 @@
+package main
+
+import "fmt"
+
+func main() {
+	names := [...]string{"Eko", "Kurniawan", "Khannedy", "Joko", "Budi", "Nugraha"}
+
+	slice1 := names[4:6]
+	fmt.Println(slice1)
+
+	slice2 := names[:3]
+	fmt.Println(slice2)
+
+	var slice3 []string = names[3:]
+	fmt.Println(slice3)
+
+	var slice4 []string = names[:]
+	fmt.Println(slice4)
+
+	days := [...]string{"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu"}
+
+	daySlice1 := days[5:] // Sabtu, Minggu
+	fmt.Println(daySlice1)
+
+	daySlice1[0] = "Sabtu Baru"
+	daySlice1[1] = "Minggu Baru"
+	fmt.Println(daySlice1)
+	fmt.Println(days)
+
+	daySlice2 := append(daySlice1, "Libur Baru")
+	daySlice2[0] = "Sabtu Lama"
+	// daysBaru := [...]string{"Senin", "Selasa", "Rabu", "Kamis", "Jumat", "Sabtu", "Minggu", "Libur Baru"}
+	fmt.Println(daySlice1)
+	fmt.Println(daySlice2)
+	fmt.Println(days)
+
+	var newSlice []string = make([]string, 2, 5)
+	newSlice[0] = "Eko"
+	newSlice[1] = "Eko"
+	// newSlice[2] = "Eko" // error, harusnya menggunakan append
+
+	fmt.Println(newSlice)
+	fmt.Println(len(newSlice))
+	fmt.Println(cap(newSlice))
+
+	newSlice2 := append(newSlice, "Eko")
+	fmt.Println(newSlice2)
+	fmt.Println(len(newSlice2))
+	fmt.Println(cap(newSlice2))
+
+	newSlice2[0] = "Budi"
+	fmt.Println(newSlice2)
+	fmt.Println(newSlice)
+
+	fromSlice := days[:]
+	toSlice := make([]string, len(fromSlice), cap(fromSlice))
+
+	copy(toSlice, fromSlice)
+
+	fmt.Println(fromSlice)
+	fmt.Println(toSlice)
+
+	iniArray := [...]int{1, 2, 3, 4, 5}
+	iniSlice := []int{1, 2, 3, 4, 5}
+
+	fmt.Println(iniArray)
+	fmt.Println(iniSlice)
+}

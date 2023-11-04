dc5daacd6faa7d6bccdeef76125adc35dd9eb610
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-04 09:01:04 +0700

    variable

---
+++ variable.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Eko Kurniawan"
+	fmt.Println(name)
+
+	name = "Eko Khannedy"
+	fmt.Println(name)
+
+	var (
+		firstName  = "Eko"
+		middleName = "Kurniawan"
+		lastName   = "Khannedy"
+	)
+
+	fmt.Println(firstName)
+	fmt.Println(middleName)
+	fmt.Println(lastName)
+}

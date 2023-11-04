6743ca5d7f4f1f600c851993ab05af080102a552
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-04 13:00:12 +0700

    tipe data array

---
+++ array.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	var names [3]string
+
+	names[0] = "Eko"
+	names[1] = "Kurniawan"
+	names[2] = "Khannedy"
+
+	fmt.Println(names[0])
+	fmt.Println(names[1])
+	fmt.Println(names[2])
+
+	var values = [...]int{
+		90,
+		80,
+		95,
+		100,
+		110,
+	}
+
+	fmt.Println(values)
+	fmt.Println(values[0])
+	fmt.Println(values[1])
+	fmt.Println(values[2])
+
+	fmt.Println(len(values))
+}

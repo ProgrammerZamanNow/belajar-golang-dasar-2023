e25ffba4c5eca1ad0c4e69e0305a83690eaefd61
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-04 09:27:42 +0700

    konversi tipe data

---
+++ conversion.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main() {
+	var nilai32 int32 = 32768
+	var nilai64 int64 = int64(nilai32)
+	var nilai16 int16 = int16(nilai32)
+
+	fmt.Println(nilai32)
+	fmt.Println(nilai64)
+	fmt.Println(nilai16)
+
+	var name = "Eko Kurniawan"
+	var e uint8 = name[0]
+	var eString = string(e)
+
+	fmt.Println(name)
+	fmt.Println(e)
+	fmt.Println(eString)
+}

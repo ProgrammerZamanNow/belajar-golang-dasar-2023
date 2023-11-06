9784b7263147a36d23225eb938642222827be5b3
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-06 18:32:49 +0700

    anonymous function

---
+++ anonymous_function.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+type Blacklist func(string) bool
+
+func registerUser(name string, blacklist Blacklist) {
+	if blacklist(name) {
+		fmt.Println("You are blocked", name)
+	} else {
+		fmt.Println("Welcome", name)
+	}
+}
+
+func main() {
+	blacklist := func(name string) bool {
+		return name == "anjing"
+	}
+	registerUser("eko", blacklist)
+
+	registerUser("anjing", func(name string) bool {
+		return name == "anjing"
+	})
+}

28ca6dcf5aa762a50e356d7f52803317fb2f3292
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-06 22:02:14 +0700

    interface

---
+++ interface.go
@@ -0,0 +1,35 @@
+package main
+
+import "fmt"
+
+type HasName interface {
+	GetName() string
+}
+
+func SayHello(value HasName) {
+	fmt.Println("Hello", value.GetName())
+}
+
+type Person struct {
+	Name string
+}
+
+func (person Person) GetName() string {
+	return person.Name
+}
+
+type Animal struct {
+	Name string
+}
+
+func (animal Animal) GetName() string {
+	return animal.Name
+}
+
+func main() {
+	person := Person{Name: "Eko"}
+	SayHello(person)
+
+	animal := Animal{Name: "Kucing"}
+	SayHello(animal)
+}

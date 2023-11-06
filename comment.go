f126fb5a616107a9c4f7d75045c151948158329b
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-06 18:58:17 +0700

    komentar

---
+++ comment.go
@@ -0,0 +1,13 @@
+package main
+
+/*
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+multiline comment
+*/
+func main() {
+	// this is single line comment
+}

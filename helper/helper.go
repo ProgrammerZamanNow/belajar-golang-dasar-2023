2c2c59f9e90b4bc9086d0be233395c4cf256150f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-06 22:59:01 +0700

    package dan import

---
+++ helper/helper.go
@@ -0,0 +1,5 @@
+package helper
+
+func SayHello(name string) string {
+	return "Hello " + name
+}
---
+++ import.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"belajar-golang-dasar/helper"
+	"fmt"
+)
+
+func main() {
+	result := helper.SayHello("Eko")
+	fmt.Println(result)
+}

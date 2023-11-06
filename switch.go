a1c89b32c6841f5ca2909d1e698b043cb66cea6f
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-06 15:37:17 +0700

    switch

---
+++ switch.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+func main() {
+	name := "Eko"
+
+	switch name {
+	case "Eko":
+		fmt.Println("Hello Eko")
+	case "Budi":
+		fmt.Println("Hello Budi")
+	default:
+		fmt.Println("Hi, Boleh Kenalan?")
+	}
+
+	switch length := len(name); length > 5 {
+	case true:
+		fmt.Println("Nama terlalu panjang")
+	case false:
+		fmt.Println("Nama sudah benar")
+	}
+
+	name = "KhannedyKhannedyKhannedy"
+	length := len(name)
+	switch {
+	case length > 10:
+		fmt.Println("Nama terlalu panjang")
+	case length > 5:
+		fmt.Println("Nama lumayan panjang")
+	default:
+		fmt.Println("Nama sudah benar")
+	}
+}

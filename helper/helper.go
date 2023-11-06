ff1961f230d6949f955182c24194ebabde1846c8
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-11-06 23:03:34 +0700

    access modifier

--- helper/helper.go
+++ helper/helper.go
@@ -1,5 +1,19 @@
 package helper
 
+import "fmt"
+
+var version = "1.0.0"
+var Application = "golang"
+
+func sayGoodBye(name string) string {
+	return "Good Bye " + name
+}
+
 func SayHello(name string) string {
 	return "Hello " + name
+}
+
+func Contoh() {
+	sayGoodBye("Eko")
+	fmt.Println(version)
 }
--- import.go
+++ import.go
@@ -8,4 +8,8 @@ import (
 func main() {
 	result := helper.SayHello("Eko")
 	fmt.Println(result)
+
+	fmt.Println(helper.Application)
+	// fmt.Println(helper.version) // tidak bisa diakses
+	// fmt.Println(helper.sayGoodBye("Eko")) // tidak bisa diakses
 }
